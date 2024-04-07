0885c9ddcea63010360eac046da415bd3af618f5
codewithsitangshu <qa.edges@gmail.com>
2024-04-07 15:38:11 +0530

    Update List Test

--- src/test/java/org/codewithsitangshu/simple/ListTest.java
+++ src/test/java/org/codewithsitangshu/simple/ListTest.java
@@ -2,8 +2,6 @@ package org.codewithsitangshu.simple;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +13,8 @@ public class ListTest {
     public void test(List<String> actual) {
 
         List<String> expected = Arrays.asList("car","ball");
+        List<String> expectedWithSameOrder = Arrays.asList("ball","apple","car");
+        List<String> expectedWithAnyOrder = Arrays.asList("car","ball","apple");
 
         assertThat(actual)
                 .hasSizeGreaterThanOrEqualTo(2) // Size of the list
@@ -22,6 +22,10 @@ public class ListTest {
                 .doesNotContain("cat") // validate it should not contain cat
                 .contains("car") // should contain car as one element
                 .containsAll(expected) //should contain all element of expected list
+                .containsExactly(expectedWithSameOrder.toArray(new String[expectedWithSameOrder.size()]))
+                .containsExactlyInAnyOrder(expectedWithAnyOrder.toArray(new String[expectedWithAnyOrder.size()]))
+                .containsExactlyElementsOf(expectedWithSameOrder)
+                .containsExactlyInAnyOrderElementsOf(expectedWithAnyOrder)
                 .allSatisfy(str -> assertThat(str.length()).isGreaterThanOrEqualTo(3)); // satisfy for all elements
 
 
